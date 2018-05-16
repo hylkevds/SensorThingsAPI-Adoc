edc4536bac1208c19398b1846e79b365c5e1f885
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-05-16 09:39:36 +0200

    Removed "r6" suffix from generated file name.

--- src/asciidoctor.sh
+++ src/asciidoctor.sh
@@ -4,7 +4,7 @@ echo "Working on 15-078..."
 echo "Genering Table of Figures..."
 ./generate_tof.rb 15-078/OGC_15-078.adoc > 15-078/pre_0_tof.adoc
 echo "Genering HTML..."
-asciidoctor -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078r6.html
+asciidoctor -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078.html
 echo "Genering PDF..."
-asciidoctor-pdf -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078r6.pdf
+asciidoctor-pdf -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078.pdf
 echo "Done."
