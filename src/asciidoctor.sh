344a8e6b35867d31836aa4e0ca69ec524f3d0042
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-09-13 14:43:29 +0200

    Try building with travis

--- src/asciidoctor.sh
+++ src/asciidoctor.sh
@@ -4,7 +4,7 @@ echo "Working on 15-078..."
 echo "Genering Table of Figures..."
 ./generate_tof.rb 15-078/OGC_15-078.adoc > 15-078/pre_0_tof.adoc
 echo "Genering HTML..."
-asciidoctor -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078.html
+asciidoctor -a allow-uri-read -o ../dist/15-078/15-078.html 15-078/OGC_15-078.adoc
 echo "Genering PDF..."
-asciidoctor-pdf -a allow-uri-read 15-078/OGC_15-078.adoc -o ../dist/15-078/15-078.pdf
+asciidoctor-pdf -a allow-uri-read -o ../dist/15-078/15-078.pdf 15-078/OGC_15-078.adoc
 echo "Done."
