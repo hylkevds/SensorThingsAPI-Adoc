9dddcbaf396222af3d3c5bd12b4cd68f7e815071
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-09-13 14:55:01 +0200

    Travis script fixes

--- src/asciidoctor.sh
+++ src/asciidoctor.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 #
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+cd $SCRIPTPATH
 echo "Working on 15-078..."
 echo "Genering Table of Figures..."
 ./generate_tof.rb 15-078/OGC_15-078.adoc > 15-078/pre_0_tof.adoc
